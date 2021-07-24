8472f5950380834c295f33be9d3fdc15b3c8be1d
Felipe Nascimento Santos <felipe.nascimento@helpper.com.br>
2021-07-24 14:28:57 -0400

    refactor: refatoraçao do AutoComplete do Google Maps

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -23,17 +23,8 @@ export class FormComponent {
 	async ngAfterViewInit(): Promise<void> {
 		const input = this.document.getElementById('autocomplete') as HTMLInputElement;
 		if(input) {
-			const center = { lat: 20.5072637, lng: -54.6643489 };
-			// Create a bounding box with sides ~10km away from the center point
-			const defaultBounds = {
-				north: center.lat + 0.1,
-				south: center.lat - 0.1,
-				east: center.lng + 0.1,
-				west: center.lng - 0.1,
-			};
 			const options = {
-				bounds: defaultBounds,
-				componentRestrictions: { country: 'pt' },
+				componentRestrictions: { country: 'br' },
 				fields: ['name'],
 				strictBounds: false,
 				types: ['establishment'],
